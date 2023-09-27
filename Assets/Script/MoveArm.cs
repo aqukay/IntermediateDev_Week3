415bce94bfbc4bb1898a33f32cfa1bde4d76e5ad
LAPTOP-SQ03A8DH\Amber <Amberqu.q2@gmail.com>
2023-09-26 21:50:49 -0400

    Tentative Timer and Arm Movement Script

--- Assets/Script/MoveArm.cs
+++ Assets/Script/MoveArm.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class MoveArm : MonoBehaviour
 {
-    public Rigidbody2D myBody;
-    SoundManager mySoundManager;
+    Rigidbody2D myBody;
 
-    float power = 200f;
+    float armPower = 200f;
+
+    Vector3 leftArmMove = new Vector3(-1, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         myBody = GetComponent<Rigidbody2D>();
-        mySoundManager = GetComponent<SoundManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        //if i press the A key
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            mySoundManager.myAudioSource.PlayOneShot(mySoundManager.armSound);
-            myBody.AddForce(transform.up * power, ForceMode2D.Impulse);
+            myBody.AddForce(leftArmMove * armPower, ForceMode2D.Impulse);
         }
     }
 }
